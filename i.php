8d30350260cc655f48ab4fc1e9c8865dd5ded118
cordanius240 <vantedf@mail.ru>
2023-08-28 02:34:45 +0400

    delete comm

--- i.php
+++
@@ -1,8 +0,0 @@
-<?php
-// if (extension_loaded('sqlsrv')) {
-//     echo 'Драйвер SQLSRV для PHP установлен.';
-// } else {
-//     echo 'Драйвер SQLSRV для PHP не установлен.';
-// }
-phpinfo()
-    ?>
